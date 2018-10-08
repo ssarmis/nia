ff58b6cddf74007425ebb3aed66d1e0d7a16cdd5
streanga-sarmis <streangasarmis@gmail.com>
2018-10-08 21:55:18 +0300

    No need for the script files anymore

--- build.bat
+++
@@ -1,6 +0,0 @@
-: g++ -o a src/*.cpp -Iinclude/ -lopengl32 -lgdi32
-g++ -c -DNIA_LIBRARY_BUILD src/*.cpp src/arch/win32/*.cpp -Iinclude/
-: in case I forget, I link opengl here because I use it internally
-g++ -shared -o nia.dll nia_event.o nia_window.o nia_renderer.o -lopengl32 -lgdi32
-: I link opengl here as well because I might want to use some after the library was made
-g++ -o a test.cpp -L. -lnia -Iinclude/ -lopengl32
