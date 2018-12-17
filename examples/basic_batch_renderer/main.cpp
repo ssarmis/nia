20c2d16c37cdd116a8b00007726374e0f53b9f98
streanga-sarmis <streangasarmis@gmail.com>
2018-12-17 18:43:38 +0200

    Made the first exmaple a bit more interesting

--- examples/basic_batch_renderer/main.cpp
+++ examples/basic_batch_renderer/main.cpp
@@ -7,33 +7,96 @@
 
 #include <GL/glext.h>
 
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float vx;
+    float vy;
+    float x;
+    float y;
+    float w;
+    float h;
+    float r;
+    float g;
+    float b;
+} rect;
+
+rect rectangles[1024 * 1024];
 
 int main() {
 
     niaWindow window;
-    window.createWindow(768, 768, "no matter");
+    window.createWindow(1024, 1024, "no matter");
 
     niaLoadEverything();
 
     window.enableVsync();
     
+
     niaBatchRenderer renderer;
 
+    srand(time(NULL));
+    int i = 0;
+    for (float y = -5; y < 5; y += 0.03){
+        for (float x = -5; x < 5; x += 0.03){
+            rectangles[i].r = ((double) rand() / (RAND_MAX));
+            rectangles[i].g = ((double) rand() / (RAND_MAX));
+            rectangles[i].b = ((double) rand() / (RAND_MAX));
+
+            float speed = ((double) rand() / (RAND_MAX)) * 0.005;
+
+            rectangles[i].vx = rand() % 2 ? speed : -speed;
+            rectangles[i].vy = rand() % 2 ? speed : -speed;
+
+            float xx = -1 + 2 * ((float)rand()) / RAND_MAX;
+            float yy = -1 + 2 * ((float)rand()) / RAND_MAX;
+
+            rectangles[i].x = xx;
+            rectangles[i].y = yy;
+
+            rectangles[i].w = 0.027;
+            rectangles[i].h = 0.027;
+
+            ++i;
+        }
+    }
+    
+    printf("Quads count: %d\n", i);
+    
     niaEvent event;
     while(!window.isClosed()){
         window.handleEvents(event);
 
         glClearColor(1, 0, 1, 1);
         glClear(GL_COLOR_BUFFER_BIT);
-     
-        for (float y = -1; y < 1; y += 0.03){
-            for (float x = -1; x < 1; x += 0.03){
-                r32 colors[3];
-                colors[0] = 0;
-                colors[1] = 0;
-                colors[2] = 1;
-                renderer.renderRectangle(x, y, 0.027, 0.027, colors);
+
+        int i = 0;
+        for (float y = -5; y < 5; y += 0.03){
+            for (float x = -5; x < 5; x += 0.03){
+                rect *r = &rectangles[i++];
+
+                bool xr = r->x + r->w / 2 >= 1;
+                bool xl = r->x - r->w / 2 <= -1;
+
+                if (xr || xl){
+                   r->vx *= -1;
+                }
+
+                bool yd = r->y - r->h / 2 <= -1;
+                bool yu = r->y + r->h / 2 >= 1;
+
+                if (yd || yu){
+                   r->vy *= -1;
+                }
+
+                float colors[] = {r->r, r->g, r->b};
+
+                r->x += r->vx;
+                r->y += r->vy;
+                renderer.renderRectangle(r->x, r->y, 
+                                        r->w, r->h, colors);
             }
         }
 
