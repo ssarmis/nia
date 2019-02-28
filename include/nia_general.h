641faa863f68809c0086be9629dc7b5b75b52887
streanga-sarmis <streangasarmis@gmail.com>
2019-02-28 22:18:15 +0200

    Added signed types

--- include/nia_general.h
+++ include/nia_general.h
@@ -22,6 +22,10 @@
 #define NIA_INTERNAL
 #define NIA_STATIC static
 
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
