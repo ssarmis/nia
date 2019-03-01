1cb93c37078dfbdd2eeb1915866d12d0c7fa8e07
streanga-sarmis <streangasarmis@gmail.com>
2019-03-01 19:21:26 +0200

    Small constants header

---
+++ include/nia_constants.h
@@ -0,0 +1,9 @@
+#ifndef _NIA_CONSTANTS_H_
+#define _NIA_CONSTANTS_H_
+
+#define NIA_VERTEX_STRIDE   0
+#define NIA_COLOR_STRIDE    (3 * sizeof(r32))
+#define NIA_NORMALS_STRIDE  (6 * sizeof(r32))
+#define NIA_UV_STRIDE       (9 * sizeof(r32))
+
+#endif //_NIA_CONSTANTS_H_
