779517ae0c83276384d71e6e84a4ebe11afd78f2
streanga-sarmis <streangasarmis@gmail.com>
2019-05-06 21:37:00 +0300

    Tried to port texture streaming ot linux, incomplete

--- src/nia_texture_streaming.cpp
+++ src/nia_texture_streaming.cpp
@@ -1,7 +1,12 @@
-#ifdef _WIN32 //tmp
-
+// TODO this should be a hole different file for a different architecture, but I have no time
 #include "nia_texture_streaming.h"
+
+#ifdef _WIN32
 #include "nia_wgl.h"
+#elif defined __unix__
+#include "nia_xgl.h"
+#endif
+
 
 textureLiveLoadingStream niaTextureStreaming::liveLoadingStream;
 
@@ -9,17 +14,36 @@ NIA_CALL void niaTextureStreaming::initializeStream(){
     liveLoadingStream.head = NULL;
     liveLoadingStream.tail = NULL;
 
+#ifdef _WIN32
     DWORD liveLoadingThreadId;
     HANDLE liveLoadingThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)(&(niaTextureStreaming::updateStreams)), NULL, 0, &liveLoadingThreadId);
+#elif defined __unix__
+    pthread_t thread;
+    pthread_create(&thread, NULL, &(niaTextureStreaming::updateStreams), NULL);
+#endif
 }
 
-NIA_CALL void niaTextureStreaming::updateStreams(){
+NIA_CALL 
+#ifdef _WIN32
+    void
+#elif defined __unix__
+    void*
+#endif
+    niaTextureStreaming::updateStreams(void*){
+#ifdef _WIN32
     wglMakeCurrent(niaWindow::deviceContext, niaWindow::glRenderContextSecond);
+#elif defined __unix__
+    glXMakeCurrent(niaWindow::display, niaWindow::window, niaWindow::openglContextSecond);
+#endif
     
     while(!niaWindow::closed){
         textureLiveLoadingChunk* iterator = liveLoadingStream.head;
         textureLiveLoadingChunk* chunk;
+
+        u32 minute;
+        u32 second;
         while(iterator){
+#ifdef _WIN32
             SYSTEMTIME utcTime;
             SYSTEMTIME localTime;
 
@@ -29,17 +53,31 @@ NIA_CALL void niaTextureStreaming::updateStreams(){
 
             FileTimeToSystemTime(&iterator->writeTime, &utcTime);
             SystemTimeToTzSpecificLocalTime(NULL, &utcTime, &localTime);
-            
-            if(iterator->writeMinute != localTime.wMinute || iterator->writeSecond != localTime.wSecond){
-                niaTextureStreaming::updateTexture(*iterator, fileHandle);
+
+            minute = localTime.wMinute;
+            second = localTime.wSecond;
+#elif defined __unix__
+            stat(iterator->texturePath, &iterator->writeStat);
+            struct tm localTime = *gmtime(&iterator->writeStat.st_mtime);
+
+            minute = localTime.tm_min;
+            second = localTime.tm_sec;
+            printf("second %d\n", second);
+#endif
+            if(iterator->writeMinute != minute || iterator->writeSecond != second){
+                niaTextureStreaming::updateTexture(*iterator);
             }
 
-            iterator->writeMinute = localTime.wMinute;
-            iterator->writeSecond = localTime.wSecond;
+            iterator->writeMinute = minute;
+            iterator->writeSecond = second;
 
             iterator = iterator->next;
         }
+#ifdef _WIN32
         Sleep(1000);
+#elif defined __unix__
+        usleep(10000);
+#endif
     }
 
     textureLiveLoadingChunk* iterator = liveLoadingStream.head;
@@ -59,10 +97,13 @@ NIA_CALL void niaTextureStreaming::appendLiveLoadingTexture(u32 textureId, char*
     chunk->texturePath = filename;
     chunk->details = details;
     chunk->next = NULL;
-
+#ifdef _WIN32
     HANDLE fileHandle = CreateFileA(chunk->texturePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
     BOOL status = GetFileTime(fileHandle, NULL, NULL, &chunk->writeTime);
     CloseHandle(fileHandle);
+#elif defined __unix__
+    // TODO 
+#endif
 
     if(liveLoadingStream.head){
         liveLoadingStream.tail->next = chunk;
@@ -74,7 +115,7 @@ NIA_CALL void niaTextureStreaming::appendLiveLoadingTexture(u32 textureId, char*
     chunk->next = NULL;
 }
 
-NIA_CALL void niaTextureStreaming::updateTexture(const textureLiveLoadingChunk& chunk, HANDLE fileHandle){
+NIA_CALL void niaTextureStreaming::updateTexture(const textureLiveLoadingChunk& chunk){
     NIA_GL_CALL(glBindTexture(GL_TEXTURE_2D, chunk.textureId));
     
 
@@ -102,4 +143,3 @@ NIA_CALL void niaTextureStreaming::updateTexture(const textureLiveLoadingChunk& 
     }
     NIA_GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
 }
-#endif
