9f46634cd843d46ae5403fe76cca7a75d124008a
streanga-sarmis <streangasarmis@gmail.com>
2019-05-21 19:18:55 +0300

    Added stuff to benchmarking

--- benchmark/basic_rectangle/nia.cpp
+++ benchmark/basic_rectangle/nia.cpp
@@ -8,23 +8,39 @@ niaWindow* window;
 static void BM_RENDER_RECTANGLE(benchmark::State& state){
     niaRenderer renderer;
 
+    glDrawBuffer(GL_FRONT);
     while(state.KeepRunning()){
         renderer.renderRectangle(0, 0, 100, 100);
-        window->swapBuffers();
+        // window->swapBuffers();
     }
 }
 BENCHMARK(BM_RENDER_RECTANGLE);
 
 static void BM_RENDER_TEXTURED_RECTANGLE(benchmark::State& state){
     niaRenderer renderer;
+    niaMesh mesh = niaMesh::quad(1.0);
     niaTexture texture("test.bmp");
-
+    niaShader shader;
     while(state.KeepRunning()){
-        renderer.renderMesh(niaMesh::quad(1.0), texture);
-        window->swapBuffers();
+        shader.useShader();
+        // renderer.renderMesh(mesh, texture);
+        renderer.renderRectangleRaw(0, 0, 100, 100, texture);
+        shader.unuseShader();
+        // window->swapBuffers();
     }
 }
-// BENCHMARK(BM_RENDER_TEXTURED_RECTANGLE);
+BENCHMARK(BM_RENDER_TEXTURED_RECTANGLE);
+
+niaTexture* texture;
+static void BM_RENDER_ANIMATED_TEXTURED_RECTANGLE(benchmark::State& state){
+    niaRenderer renderer;
+    niaMesh mesh = niaMesh::quad(1.0);
+    while(state.KeepRunning()){
+        renderer.renderMesh(mesh, *texture);
+        // window->swapBuffers();
+    }
+}
+BENCHMARK(BM_RENDER_ANIMATED_TEXTURED_RECTANGLE);
 
 // static void BM_RENDER_ANIMATED_TEXTURED_RECTANGLE(benchmark::State& state){
 //     niaRenderer renderer;
@@ -37,10 +53,24 @@ static void BM_RENDER_TEXTURED_RECTANGLE(benchmark::State& state){
 // BENCHMARK(BM_RENDER_ANIMATED_TEXTURED_RECTANGLE)->Arg(1);
 
 int main(int argc, char* argv[]){
-    window->createWindow(640, 480, "Create Rectangle Benchmark");
+    window->createWindow(640, 480, "Nia Benchmark");
 
     niaLoader::loadEverything();
-   
+    texture = new niaTexture("test.gif");
+    // niaRenderer renderer;
+    // renderer.pushPerspectiveView(60, 1.66, 0.01, 1000);
+    // niaTransform transform;
+    // transform.translate(niaVector3<float>(0, 0, -10));
+    // niaEvent event;
+    // while(!window->isClosed()){
+    //     window->handleEvents(event);
+    //     renderer.submitTransformation(transform);
+    //     r32 colors[] = {0, 1, 0};
+    //     // renderer.renderRectangle(0, 0, 100, 100, colors);
+    //     renderer.renderMesh(niaMesh::quad(1.0));
+    //     window->swapBuffers();
+    // }
+
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
     return 0;
