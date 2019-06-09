3c48aea3e2b340a37453aadf5ca9b108d59614c0
streanga-sarmis <streangasarmis@gmail.com>
2019-06-09 13:04:00 +0300

    Forgot an event header

--- include/arch/win32/nia_event.h
+++ include/arch/win32/nia_event.h
@@ -17,11 +17,63 @@
 #include <windows.h>
 
 #include "nia_general.h"
+#include "nia_vector.h"
+
+NIA_STRUCT keyboardDetails {
+    u32 activeKey;
+    u32 oldActiveKey;
+    u32 lastKeyboardEvent;
+} keyboardDetails;
+
+NIA_STRUCT mouseDetails {
+    bool leftMouseClicked;
+    bool rightMouseClicked;
+    i32 x;
+    i32 y;
+} mouseDetails;
 
 NIA_CLASS niaEvent {
+private:
+    // TODO implement this array into detecting when a key was pressd nicely
+    bool keyCompletePress[256];
+    u32 eventType;
+    keyboardDetails keyboard;
+    mouseDetails mouse;
+
 public:
     NIA_CALL niaEvent();
     NIA_CALL ~niaEvent();
+
+    NIA_INLINE u32 NIA_CALL getEventType() const {
+        return eventType;
+    }
+
+    NIA_INLINE u32 NIA_CALL getActiveKey(){
+        return keyboard.activeKey;
+    }
+
+    // returns the last keyt that was pressed completely
+    NIA_INLINE bool NIA_CALL getCompleteKeyPress(u32 key){
+        return keyCompletePress[key];
+    }
+
+    NIA_INLINE i32 NIA_CALL getMouseX(){
+        return mouse.x;
+    }
+
+    NIA_INLINE i32 NIA_CALL getMouseY(){
+        return mouse.y;
+    }
+
+    NIA_INLINE bool NIA_CALL getMouseLeftButton(){
+        return mouse.leftMouseClicked;
+    }
+
+    NIA_INLINE bool NIA_CALL getMouseRightButton(){
+        return mouse.rightMouseClicked;
+    }
+
+    bool NIA_CALL processMessageReceived();
 
     MSG msg;
 };
